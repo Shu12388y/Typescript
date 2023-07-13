f04e853429b568dd61211698a26df22d4ee3c893
Shu12388y <spl.sp999@gmail.com>
2023-07-13 17:13:13 +0530

    types folder is added

---
+++ types/index.ts
@@ -0,0 +1,65 @@
+// Let us define a number. A number type can be postive, negative, floating, integer
+let postiveNumber:number=9;
+let negativeNumber:number=-8;
+let floatNumber:number=3.12;
+
+
+// In the above code we have define the number tyoes
+
+
+// Now let us define some string
+
+
+let named:string="shubham";
+let fullName:string="shubham Paul";
+let lastName:string="Paul";
+let address:string="India"
+
+// In the above content we have init the string types
+
+
+// TODO 1: we have to concat two stringd
+
+
+let newConcat:string=`Hello my name is${fullName}. My address is ${address}`
+console.log(newConcat)
+
+
+
+// TODO 2: we have to find the length of the string
+
+let newLength:number=named.length
+console.log(newLength)
+
+// TODO 3: Convert the string to uppercase and lowercase
+
+
+let uppercaseText:string=named.toUpperCase()
+let lowercaseText:string=named.toLowerCase()
+
+
+// TODo 4: to substring
+
+let strindSent:string="Hello we have a new car";
+
+let subString:string=strindSent.substring(0,3)
+console.log(subString)
+
+
+// TODO 5: string compare
+
+
+let compValue:boolean;
+if(uppercaseText===lowercaseText){
+    compValue=true
+    console.log(compValue)
+}
+else{
+    compValue=false
+    console.log(compValue)
+}
+
+
+
+
+
---
+++ types/readme.md
@@ -0,0 +1,20 @@
+# Intrioduction To TypeScript
+## In typescript, type annotation is a way of expilicitly specifying the type of a variable, function parameter, or function return value
+
+
+# Types in TypeScript
+1. NUMBER
+2. BIGLNT
+3. BOOLEAN
+4. STRING
+5. SYMBOL
+6. OBJECT
+
+
+## In string we have sub types that is STRINGENUM
+## In symbol we have sub type that is UNIQUESYMBOL
+## In the object we have 
+1. Array in array we have Tuple 
+2. function
+3. constructor
+## In number we have sub types that is NUMBEREMUN
