0c95b27024252469b8b32b84fa8e8e2d04a125fc
Shu12388y <spl.sp999@gmail.com>
2023-07-22 19:46:59 +0530

    object file is added:

---
+++ objects/object.ts
@@ -0,0 +1,37 @@
+// object are used to represent key value pair
+// create a object
+const person:{
+  name:string,
+  age:number,
+  isStudent:boolean,
+  address:{
+    city:string,
+    college:string,
+    country:string
+  }
+}={
+  name:"shubham",
+  age:20,
+  isStudent:false,
+  address:{
+    city:"jamshedpur",
+    college:"C.V. Raman Global University",
+    country:"India"
+  }
+} 
+
+
+
+// TODO 1: make an object of product which contain name, price, quantity
+//
+
+
+const product:{
+name:string,
+price:number,
+quantity:number
+}={
+name:"Laptop",
+price:23000,
+quantity:1
+}
