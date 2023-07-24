b0a50bf1948e97479fc22b06d2de57989e7028ce
Shu12388y <spl.sp999@gmail.com>
2023-07-24 20:18:42 +0530

    file are added

---
+++ objects/callSignature.ts
@@ -0,0 +1,4 @@
+//? call signature
+//? The function call signature refer to the declaration or definition of the funciton which includes the function's name, parameter, and return type
+//? It define the structure and type information of the function wihtout including the function's implementation or body.
+//? It is more like creating a method in object
